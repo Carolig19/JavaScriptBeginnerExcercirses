e47fc02193c8284c2aa57b2ece410150e3cfc6d6
Carolig19 <ciribarrenguerra@gmail.com>
2022-02-15 23:10:49 +0000

    Ejercicios JS

--- exercises/14-Your-first-switch/app.js
+++ exercises/14-Your-first-switch/app.js
@@ -1,17 +1,29 @@
-function getColor(selection)
-{
-	switch(selection){
-		// Add more options here
-	    default:
-	    	return false;  //returns false because the user picked an unavailable color
-	    break;               
+function getColor(selection) {
+	
+	switch (selection) {
+		case 1:
+			color = 'rojo';
+			return "true";
+			break;
+		case 2:
+			color = 'azul';
+			return "true";
+			break;
+		case 3:
+			color = 'verde';
+			return "true";
+			break;
+
+		default:
+			return false;  //returns false because the user picked an unavailable color
+			break;
 	}
 }
 
 var colorname = prompt('What color do you want?').trim();
 var isAvailable = getColor(colorname);
 
-if(isAvailable)
+if (isAvailable)
 	console.log('Good news! That color is available');
-else 
+else
 	console.log('We are sorry, that color is not available');
